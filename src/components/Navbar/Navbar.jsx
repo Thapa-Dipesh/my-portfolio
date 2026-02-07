a1db078f83bd1c0ba0b62e02cbb7def7d9727144
Dipesh Thapa <dthapa4682@gmail.com>
2026-02-07 20:47:06 +0545

    navbar mixed for mobile menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -41,7 +41,7 @@ const Navbar = () => {
       {/* Mobile Menu Overlay */}
       <div
         className={`${
-          isOpen ? "translate-x-0" : "translate-x-full"
+          isOpen ? "translate-x-0" : "hidden"
         } fixed top-0 right-0 h-screen w-3/4 bg-[#111422] shadow-2xl transition-transform duration-300 ease-in-out md:hidden flex flex-col items-center justify-center gap-10 z-40`}
       >
         {["About", "Skills", "Projects", "Contact"].map((item) => (
